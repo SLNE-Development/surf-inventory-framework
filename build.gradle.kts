0e7ba569d325c1fb97f3f1948282fc0b1b084a75
twisti <76837088+twisti-dev@users.noreply.github.com>
2026-05-14 13:02:11 +0200

    ✨ feat(build): update project configuration for Java 25 and dependencies
    - upgrade Gradle wrapper from 8.14.3 to 9.5.1
    - set Java source and target compatibility to 25
    - update Spigot and PaperSpigot versions to 1.21.11-R0.1-SNAPSHOT
    - upgrade Kotlin version from 1.9.20 to 2.3.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,24 @@ plugins {
     id("ca.stellardrift.gitpatcher") version "2.0.0"
 }
 
-group = "dev.slne.forks.inventoryframework"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "dev.slne.forks.inventoryframework"
+    version = "1.0-SNAPSHOT"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+}
+
+gitPatcher.patchedRepos {
+    register("inventory-framework") {
+        submodule = "upstream"
+        target = file("inventory-framework")
+        patches = file("patches")
+    }
 }
 
 java {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,11 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 rootProject.name = "surf-inventory-framework"
+
+if (file("inventory-framework/build-logic/settings.gradle.kts").exists()) {
+    includeBuild("inventory-framework/build-logic")
+}
+
+if (file("inventory-framework/settings.gradle.kts").exists()) {
+    includeBuild("inventory-framework")
+}
